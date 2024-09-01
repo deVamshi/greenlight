901e67c66970e973dda848c9b96db225d9738c73
deVamshi <dev.vamshidhar@gmail.com>
2024-09-01 08:28:05 +0530

    lesson: 4.1 - decoding json req into go structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,7 +10,22 @@ import (
 )
 
 func (app *application) createMovie(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", input)
 }
 
 func (app *application) movieById(w http.ResponseWriter, r *http.Request) {
